1f4a70f1e4b704f23521b8888e3fa8fb2a629a0c
KrishnaKothandaraman <kkrishna0701@gmail.com>
2022-10-14 17:29:15 +0800

    TimeX support added

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
-#include<assert.h>
-#include<string.h>
-#include<errno.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/resource.h>
+
 
 void pointer_shift_to_left_by_one(char *a[]) {
    int i;
@@ -12,6 +14,10 @@ void pointer_shift_to_left_by_one(char *a[]) {
    }
 }
 
+
+float convert_to_seconds(int seconds, long int microseconds){
+    return (seconds + (1e-6*microseconds));
+}
 
 void display_prompt(){
     printf("3230shell ## ");
@@ -32,7 +38,7 @@ void read_commands(char cmd[], char *par[]){
         }
     }
     input[ct] = '\0';
-    printf("User entered command: %s", input);
+    printf("[INFO]: User entered command: %s", input);
 
     words = strtok(input, " \n");
 
@@ -40,13 +46,18 @@ void read_commands(char cmd[], char *par[]){
         arr[i++] = strdup(words);
         words = strtok(NULL, " \n");
     }
-    printf("Tokenized command is: ");
+    printf("[INFO]: Tokenized command is: ");
     for(int j = 0; j < i; j ++){
         printf("%s, ", arr[j]);
     }
     printf("\n");
     
-    strcpy(cmd, arr[0]);
+    if (strcmp(arr[0], "timeX") == 0){
+        strcpy(cmd, arr[1]);
+    }
+    else{
+        strcpy(cmd, arr[0]);
+    }
 
     for(j = 0;j < i; j++){
         par[j] = arr[j];
@@ -55,34 +66,42 @@ void read_commands(char cmd[], char *par[]){
 }
 
 int main(){
-    char command[200], *params[20];
+    char command[200], *params[30];
+    struct rusage usage;
+    int timeX;
     while(1){
         display_prompt();
         read_commands(command, params);
+        timeX = 0;
         // timeX variable keeps track of whether timeX was entered
-        int timeX = 0;
         if (strcmp(params[0], "timeX") == 0){
             timeX = 1;
             pointer_shift_to_left_by_one(params);
-            printf("TimeX not implemented\n");
-            break;
+            if (params[0] == NULL){
+                fprintf(stderr, "[ERROR] \"timeX\" cannot be a standalone command\n");
+            }
         }
 
         int child = fork();
         if(child == -1){
-            printf("fork() Failed! %s", strerror(errno));
+            fprintf(stderr, "[ERROR] fork() Failed! %s", strerror(errno));
             continue;
         }
-        if (child != 0){
+        if (child == 0){
             //child process. Start new process
-            printf("Child successfully created. Now running command\n");
+            printf("[INFO]: Child successfully created\n");
             if ( execvp(command, params) == -1){
-                printf("Error occur occured = %s\n", strerror(errno));
+                fprintf(stderr, "[ERROR] '%s' %s\n", command, strerror(errno));
                 continue;
             }
         }
         else{
-            wait(NULL);
+            int status;
+            waitpid(child, &status, 0);  // wait for child to terminate
+            getrusage(RUSAGE_CHILDREN, &usage);
+            if (timeX) {
+            printf("(PID)%d\t(CMD)%s\t(user)%fs\t(sys)%fs\n",child, command, convert_to_seconds(usage.ru_utime.tv_sec, usage.ru_utime.tv_usec), convert_to_seconds(usage.ru_stime.tv_sec, usage.ru_stime.tv_usec));
+            }
         }
         break;
     }
